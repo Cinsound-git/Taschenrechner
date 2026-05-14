609b7147d9d7ffb15ece51eb5c8350f2983cec71
Tim <timbartelmess@icloud.com>
2026-05-14 20:49:59 +0200

    Taschenrechner

---
+++ Taschenrechner.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double zahle[100];
+    int i = 0;
+    double ergebnis = 0;
+
+    while(t = f) {
+        printf("Gebe die Zahle ein (Bei f eingabe wird die Schleife Bendet): \n");
+        scanf("%lf",zahle[i]);
+        i++;
+        printf("Gebe die Zahle ein (Bei f eingabe wird die Schleife Bendet): \n");
+        scanf("%lf",zahle[i]);
+
+
+        if(operator = +) {
+            ergebnis = zahle[i-1] + zahle[i];
+        } else if(operator = -) {
+            ergebnis = zahle[i-1] - zahle[i];
+        } else if(operator = *) {
+            ergebnis = zahle[i-1] * zahle[i];
+        } else if(operator = /) {
+            ergebnis = zahle[i-1] / zahle[i];
+        }
+        i++;
+    }
+    printf("Ergebnis ist: %lf",ergebnis);
+}
